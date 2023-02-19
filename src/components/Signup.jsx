ca05c57bb115f970af0e2c4155b4b4dd57682cb1
iamveevs <vtv43114@uga.edu>
2023-02-18 22:36:03 -0500

    signup final

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,35 +10,35 @@ const Signup = () => {
                 <div className="column-row"></div>
                 <form>
 
-                    <h1 class="font-bold text-2xl ml-60 mt-4 text-white mt-14">User Information</h1>
+                    <h1 class="font-bold text-2xl ml-56 mt-4 text-white mt-10">User Information</h1>
 
                     <br/>
 
 				    <input class="shadow appearance-none border rounded w-50 py-2 px-2 mx-20 my-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="username" 
                     type="text" 
-                    placeholder="Username"/>
+                    placeholder="Username*"/>
 
 				    <input class="shadow appearance-none border rounded w-50 py-2 px-2 ml-1 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="phone-number" 
                     type="text" 
-                    placeholder="Phone Number"/>
+                    placeholder="Phone Number*"/>
 
                     <br/>
 
                     <input class="shadow appearance-none border rounded w-50 py-2 px-2 mx-20 my-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="password" 
                     type="text" 
-                    placeholder="Password"/>
+                    placeholder="Password*"/>
 
 				    <input class="shadow appearance-none border rounded w-50 py-2 px-2 ml-1 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="email" 
                     type="text" 
-                    placeholder="Email"/>
+                    placeholder="Email*"/>
                     
                     <br/>
 
-                    <h1 class="font-bold text-2xl ml-60 mt-4 text-white">Payment Info</h1>
+                    <h1 class="font-bold text-2xl ml-48 mt-4 text-white">Payment Info(Optional)</h1>
 
                     <br/>
 
@@ -83,31 +83,37 @@ const Signup = () => {
                     <input class="shadow appearance-none border rounded w-9/12 py-2 px-2 ml-20 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="address" 
                     type="text" 
-                    placeholder="Street Address"/>
+                    placeholder="Street Address*"/>
 
                     <input class="shadow appearance-none border rounded w-3/12 py-2 px-2 ml-20 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="city" 
                     type="text" 
-                    placeholder="City"/>
+                    placeholder="City*"/>
 
                     <input class="shadow appearance-none border rounded w-3/12 py-2 px-2 -ml-2 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="state" 
                     type="text" 
-                    placeholder="State"/>
+                    placeholder="State*"/>
 
                     <input class="shadow appearance-none border rounded w-3/12 py-2 px-2 -ml-2 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                     id="addresszip" 
                     type="text" 
-                    placeholder="Zip/Postal Code"/>
+                    placeholder="Zip/Postal Code*"/>
                 </form>
 
                 <br/>
 
-                <label class="font-bold text-1xl ml-32 mt-4 mb-14 text-white">Do you want to be signed up for email promotions?
+                <label class="font-bold text-1xl ml-32 mt-4 text-white">Do you want to be signed up for email promotions?
                 <input class="ml-1"
                 id="promotions"
                 type="checkbox"/>
                 </label>
+                <p class="font-bold text-1xl ml-40 text-white">Input fields marked with a star are required</p>
+                <br/>
+                <button type="button" 
+                class="text-white bg-gray-800 w-32 ml-64 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    Submit Form
+                </button>
 
                 <div className="column-row"></div>
             </div>
