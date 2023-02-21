10dc7fa6734ed87b0eb0f838a018d497494ee4b3
brandonho1 <brandonho00217@gmail.com>
2023-02-21 00:08:31 -0500

    OrderSummary

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./OrderSummary.css";
 
-const OrderSummary = () => {
+function OrderSummary() {
+  const [ticketPrice] = useState(10);
+  const [ticketQuantity, setTicketQuantity] = useState(1);
+
+  const handleQuantityChange = (event) => {
+    const quantity = parseInt(event.target.value);
+    setTicketQuantity(quantity);
+  };
+
+  const orderTotal = ticketPrice * ticketQuantity;
+
   return (
-	<div>OrderSummary</div>
-  )
+    <div className="order-summary">
+      <h2>Order Summary</h2>
+      <div className="order-details">
+      <div className="order-detail">
+          <span className="detail-label">Ticket Quantity:</span>
+          <input type="number" min="1" value={ticketQuantity} onChange={handleQuantityChange}/>
+        </div>
+        <div className="order-detail">
+          <span className="detail-label">Ticket Price:</span>
+          <span className="detail-value">${ticketPrice}</span>
+        </div>
+        <div className="order-detail">
+          <span className="detail-label">Order Total:</span>
+          <span className="detail-value">${orderTotal}</span>
+        </div>
+        <button className="checkout-button">Check Out</button>
+      </div>
+    </div>
+  );
 }
 
-export default OrderSummary
+export default OrderSummary;
